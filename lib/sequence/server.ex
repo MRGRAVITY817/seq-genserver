1e9c89a47cdb559169a53767b67d2e9752be3a83
Hoon Wee <mrgravity817@gmail.com>
2022-06-30 00:02:53 +0900

    refactor: separate original code to business logic + public api + server part

--- lib/sequence/server.ex
+++ lib/sequence/server.ex
@@ -2,23 +2,8 @@ defmodule Sequence.Server do
   # By using `use`, we can use the callbacks provided by GenServer
   # without explicitly declaring it. It's just like an inheritance.
   use GenServer
+  alias Sequence.Impl
 
-  #####
-  # Public API
-  def start_link(current_number) do
-    GenServer.start_link(__MODULE__, current_number, name: __MODULE__)
-  end
-
-  def next_number do
-    GenServer.call(__MODULE__, :next_number)
-  end
-
-  def increment_number(delta) do
-    GenServer.cast(__MODULE__, {:increment_number, delta})
-  end
-
-  #####
-  # GenServer Implementation
   # `init()` is a constructor of the behaviour
   def init(initial_number) do
     {:ok, initial_number}
@@ -27,12 +12,12 @@ defmodule Sequence.Server do
   # `handle_call()` will be called when client calls the server
   # Usage: `GenServer.call(pid, :next_number)`
   def handle_call(:next_number, _from, current_number) do
-    {:reply, current_number, current_number + 1}
+    {:reply, current_number, Impl.next(current_number)}
   end
 
   # should be called when `GenServer.cast()` is called
   def handle_cast({:increment_number, delta}, current_number) do
-    {:noreply, current_number + delta}
+    {:noreply, Impl.increment(current_number, delta)}
   end
 
   # This will format the `data` part of the `sys.get_status` result

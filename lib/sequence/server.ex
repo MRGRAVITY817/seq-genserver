40ade232d58442edc3611e8d1bd584cd7cb995d4
Hoon Wee <mrgravity817@gmail.com>
2022-06-29 21:47:39 +0900

    refactor(server): create public api that delegates the GenServer API

--- lib/sequence/server.ex
+++ lib/sequence/server.ex
@@ -3,6 +3,22 @@ defmodule Sequence.Server do
   # without explicitly declaring it. It's just like an inheritance.
   use GenServer
 
+  #####
+  # Public API
+  def start_link(current_number) do
+    GenServer.start_link(__MODULE__, current_number, name: __MODULE__)
+  end
+
+  def next_number do
+    GenServer.call(__MODULE__, :next_number)
+  end
+
+  def increment_number(delta) do
+    GenServer.cast(__MODULE__, {:increment_number, delta})
+  end
+
+  #####
+  # GenServer Implementation
   # `init()` is a constructor of the behaviour
   def init(initial_number) do
     {:ok, initial_number}
@@ -16,7 +32,7 @@ defmodule Sequence.Server do
 
   # should be called when `GenServer.cast()` is called
   def handle_cast({:increment_number, delta}, current_number) do
-    {:no_reply, current_number + delta}
+    {:noreply, current_number + delta}
   end
 
   # This will format the `data` part of the `sys.get_status` result
